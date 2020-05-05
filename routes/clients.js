eaf28738d4107d44c06610bab38a0ef069de95cc
Miguel <migueldelcorso@gmail.com>
2020-05-05 15:16:55 -0300

    Todos los endPoints-Falta que quede bien

--- routes/clients.js
+++ routes/clients.js
@@ -41,7 +41,10 @@ router
 		}, 100);
 	})
 	.post((req, res) => {
-		const { name, email } = req.body;
+		const {
+			name,
+			email
+		} = req.body;
 		if (name && email) {
 			client.get("clientId", (err, reply) =>
 				client.hmset(`client:${reply}`, "name", name, "email", email)
@@ -63,21 +66,26 @@ router
 
 	.get((req, res) =>
 		client.hgetall(`client:${req.params.num}`, (err, reply) =>
-			reply
-				? res.json({
-						status: "SUCCESS",
-						data: reply,
-					})
-				: res.json({
-						status: "FAILED",
-						err: errors.err2,
-					})
+			reply ?
+			res.json({
+				status: "SUCCESS",
+				data: reply,
+			}) :
+			res.json({
+				status: "FAILED",
+				err: errors.err2,
+			})
 		)
 	)
 
 	.put((req, res) => {
-		const { name, email } = req.body;
-		const { num } = req.params;
+		const {
+			name,
+			email
+		} = req.body;
+		const {
+			num
+		} = req.params;
 		client.hgetall(`client:${req.params.num}`, (err, reply) => {
 			if (reply) {
 				if (name && email)
@@ -105,29 +113,29 @@ router
 	})
 	.delete((req, res) =>
 		client.del(`client:${req.params.num}`, (err, reply) =>
-			reply
-				? res.json({
-						status: "OK",
-						msg: messages.msg1,
-					})
-				: res.json({
-						status: "FAILED",
-						err: errors.err1,
-					})
+			reply ?
+			res.json({
+				status: "OK",
+				msg: messages.msg1,
+			}) :
+			res.json({
+				status: "FAILED",
+				err: errors.err1,
+			})
 		)
 	);
 
 router
 	.route("/:num/contracts")
-	.get((req, res) =>{
-		let clientContracts=[]
-		const addClientContract=(contract)=>clientContracts.push(contract)
-		client.hgetall(`client:${req.params.num}`,(err,reply)=>{
-			reply?
-				client.get('contractId',(err,reply)=>{
-					for(let i=1;i<=reply;i++){
-						client.hgetall(`contract:${i}`,(err,reply)=>{
-							if(reply && reply.clientId===req.params.num){
+	.get((req, res) => {
+		let clientContracts = []
+		const addClientContract = (contract) => clientContracts.push(contract)
+		client.hgetall(`client:${req.params.num}`, (err, reply) => {
+			reply ?
+				client.get('contractId', (err, reply) => {
+					for (let i = 1; i <= reply; i++) {
+						client.hgetall(`contract:${i}`, (err, reply) => {
+							if (reply && reply.clientId === req.params.num) {
 								addClientContract(reply)
 							}
 						})
@@ -138,17 +146,18 @@ router
 							data: clientContracts,
 						})
 					}, 400);
-				})
-				:
+				}) :
 				res.json({
-						status: "FAILED",
-						err: errors.err2,
-					})
+					status: "FAILED",
+					err: errors.err2,
+				})
 		})
 	})
 
 	.post((req, res) => {
-		const { amount } = req.body;
+		const {
+			amount
+		} = req.body;
 		if (Number(amount) > 0) {
 			client.hgetall(`client:${req.params.num}`, (err, reply) => {
 				if (reply) {
@@ -185,51 +194,59 @@ router
 	.route("/:num/contracts/:numC")
 
 	.get((req, res) => {
-		const { num, numC } = req.params;
+		const {
+			num,
+			numC
+		} = req.params;
 		client.hgetall(`client:${num}`, (err, reply) => {
 			reply
-				? client.hgetall(`contract:${numC}`, (err, reply) =>
-						reply
-							? res.json({
-									status: "SUCCESS",
-									data: reply,
-								})
-							: res.json({
-									status: "FAILED",
-									err: errors.err4,
-								})
-					)
-				: res.json({
+				?
+				client.hgetall(`contract:${numC}`, (err, reply) =>
+					reply ?
+					res.json({
+						status: "SUCCESS",
+						data: reply,
+					}) :
+					res.json({
 						status: "FAILED",
-						err: errors.err2,
-					});
+						err: errors.err4,
+					})
+				) :
+				res.json({
+					status: "FAILED",
+					err: errors.err2,
+				});
 		});
 	})
 
 	.put((req, res) => {
-		const { num, numC } = req.params;
+		const {
+			num,
+			numC
+		} = req.params;
 		if (req.body.amount > 0) {
 			client.hgetall(`client:${num}`, (err, reply) => {
 				reply
-					? client.hgetall(`contract:${numC}`, (err, reply) => {
-							if (reply) {
-								client.hset(`contract:${numC}`, "amount", req.body.amount);
-								client.hgetall(`contract:${numC}`, (err, reply) =>
-									res.json({
-										status: "SUCCESS",
-										data: reply,
-									})
-								);
-							} else
+					?
+					client.hgetall(`contract:${numC}`, (err, reply) => {
+						if (reply) {
+							client.hset(`contract:${numC}`, "amount", req.body.amount);
+							client.hgetall(`contract:${numC}`, (err, reply) =>
 								res.json({
-									status: "FAILED",
-									err: errors.err4,
-								});
-						})
-					: res.json({
-							status: "FAILED",
-							err: errors.err2,
-						});
+									status: "SUCCESS",
+									data: reply,
+								})
+							);
+						} else
+							res.json({
+								status: "FAILED",
+								err: errors.err4,
+							});
+					}) :
+					res.json({
+						status: "FAILED",
+						err: errors.err2,
+					});
 			});
 		} else
 			res.json({
@@ -239,24 +256,27 @@ router
 	})
 
 	.delete((req, res) => {
-		const { num, numC } = req.params;
+		const {
+			num,
+			numC
+		} = req.params;
 		client.hgetall(`client:${num}`, (err, reply) =>
-			reply
-				? client.del(`contract:${numC}`, (err, reply) =>
-						reply
-							? res.json({
-									status: "OK",
-									msg: messages.msg4,
-								})
-							: res.json({
-									status: "FAILED",
-									err: errors.err4,
-								})
-					)
-				: res.json({
-						status: "FAILED",
-						err: errors.err2,
-					})
+			reply ?
+			client.del(`contract:${numC}`, (err, reply) =>
+				reply ?
+				res.json({
+					status: "OK",
+					msg: messages.msg4,
+				}) :
+				res.json({
+					status: "FAILED",
+					err: errors.err4,
+				})
+			) :
+			res.json({
+				status: "FAILED",
+				err: errors.err2,
+			})
 		);
 	});
 module.exports = router;
